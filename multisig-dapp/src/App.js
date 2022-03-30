8d648c5270196cbefe2dc2867eb487b73fe3a143
ianflexa <ianflexa@gmail.com>
2022-03-30 18:12:16 -0400

    write contract functions

--- multisig-dapp/src/App.js
+++ multisig-dapp/src/App.js
@@ -3,15 +3,17 @@ import { ethers, BigNumber } from 'ethers';
 import WalletSetup from './metamask';
 import * as x from './styles/geral'
 import ReadFunctions from './readFunctions';
+import WriteContract from './writeFunctions';
 
 function App() {
 
   return (
     <>
       <x.StyledApp>
-        <h1 style={{fontFamily: 'Segoe UI'}}>MultiSig Wallet Dapp</h1>
+        <h1>MultiSig Wallet Dapp</h1>
         <WalletSetup />
         <ReadFunctions />
+        <WriteContract />
       </x.StyledApp>
     </>
   );
